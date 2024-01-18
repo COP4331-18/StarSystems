5081735f3287e33de79c4307b1f3131f6822d09a
AustinVosler <austinpvosler@gmail.com>
2024-01-18 15:39:46 -0500

    Fixed Formatting on Login.php and AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,36 +1,41 @@
 <?php
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
+	
+	$UserID = $inData["UserID"];
+	$Name = $inData["Name"];
+	$Phone = $inData["Phone"];
+	$Email = $inData["Email"];
 
-$UserID = $inData["UserID"];
-$Name = $inData["Name"];
-$Phone = $inData["Phone"];
-$Email = $inData["Email"];
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserID,Name,Phone,Email) VALUES(?,?,?,?)");
+		$stmt->bind_param("ss", $UserID, $Name, $Phone, $Email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
 
-$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
-if ($conn->connect_error) {
-	returnWithError($conn->connect_error);
-} else {
-	$stmt = $conn->prepare("INSERT into Contacts (UserID, Name, Phone, Email) VALUES(?,?,?,?)");
-	$stmt->bind_param("ss", $UserID, $Name, $Phone, $Email);
-	$stmt->execute();
-	$stmt->close();
-	$conn->close();
-	returnWithError("");
-}
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-function getRequestInfo()
-{
-	return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson($obj)
-{
-	header('Content-type: application/json');
-	echo $obj;
-}
-
-function returnWithError($err)
-{
-	$retValue = '{"error":"' . $err . '"}';
-	sendResultInfoAsJson($retValue);
-}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,52 +1,58 @@
 
 <?php
 
-$inData = getRequestInfo();
+	$inData = getRequestInfo();
+	
+	$ID = 0;
+	$FirstName = "";
+	$LastName = "";
 
-$ID = 0;
-$FirstName = "";
-$LastName = "";
+	$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
-if ($conn->connect_error) {
-	returnWithError($conn->connect_error);
-} else {
-	$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
-	$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
-	$stmt->execute();
-	$result = $stmt->get_result();
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
 
-	if ($row = $result->fetch_assoc()) {
-		returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
-	} else {
-		returnWithError("No Records Found");
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	$stmt->close();
-	$conn->close();
-}
-
-function getRequestInfo()
-{
-	return json_decode(file_get_contents('php://input'), true);
-}
-
-function sendResultInfoAsJson($obj)
-{
-	header('Content-type: application/json');
-	echo $obj;
-}
-
-function returnWithError($err)
-{
-	$retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-	sendResultInfoAsJson($retValue);
-}
-
-function returnWithInfo($FirstName, $LastName, $ID)
-{
-	$retValue = '{"ID":' . $id . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
-	sendResultInfoAsJson($retValue);
-}
-
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $FirstName, $LastName, $ID )
+	{
+		$retValue = '{"ID":' . $id . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
