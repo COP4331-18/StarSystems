d6e89261ab574a10ba7fcca44c03fabff4180eaa
DIRHAEL <97363312+DIRHAEL@users.noreply.github.com>
2024-01-19 15:01:55 -0500

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,26 +1,24 @@
 <?php
-    $inData = getRequestInfo();
 
-    $UserID = $inData["UserID"];
-    $FirstName = $inData["FirstName"];
-    $LastName = $inData["LastName"];
-
-    $conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
-
-    if($conn->connect_error)
-    {
-        returnWithError( $conn->connect_error );
-    }
-    else
-    {
-        $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ?");
-        $stmt->bind_param("ssi", $FirstName, $LastName, $UserID);
-  	    $stmt->execute();
-  	    $stmt->close();
-  	    $conn->close();
-  	    returnWithError("");
-    }
-
+	$inData = getRequestInfo();
+	
+	$UserID = $inData["UserID"];
+	$ID = $inData["ID"];
+	
+	$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) {
+		returnWithError($conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserID = ? AND ID = ?");
+		$stmt->bind_param("ss", $UserID, $ID);
+		$stmt->execute();
+		if ($stmt->affected_rows == 0) {
+			returnWithError("Contact not found");
+		}
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
     function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
