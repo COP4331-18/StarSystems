f022b74321246f753d93ce7aa2f8a7262d87de72
Jovanny <jovannyrebollar1@gmail.com>
2024-02-03 17:15:07 -0500

    working on search functionality

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,43 +1,26 @@
 <?php
 
 	$inData = getRequestInfo();
+	
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
 
-  	$FirstName = $inData["FirstName"];
-  	$LastName = $inData["LastName"];
-  	$Login = $inData["Login"];
-  	$Password = $inData["Password"];
-
-  	$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
-  	if ($conn->connect_error) 
-  	{
-    		returnWithError($conn->connect_error);
-  	} 
-  	else 
-  	{
-    		// Check for existing user with the same Login
-    		$stmt = $conn->prepare("SELECT COUNT(*) FROM Users WHERE Login=?");
-    		$stmt->bind_param("s", $Login);
-    		$stmt->execute();
-    		$result = $stmt->get_result();
-    		$row = $result->fetch_row();
-    		$existingUserCount = $row[0];
-    		$stmt->close();
-
-    		if ($existingUserCount > 0) 
-    		{
-        		returnWithError("Username already exists");
-    		} 
-    		else 
-    		{
-        		// Proceed with registration if the username is unique
-        		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-        		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
-        		$stmt->execute();
-        		$stmt->close();
-        		$conn->close();
-        		returnWithError("");
-    		}
-  	}
+	$conn = new mysqli("localhost", "NotTheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
 
 	function getRequestInfo()
 	{
